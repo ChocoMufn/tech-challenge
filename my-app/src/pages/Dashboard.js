8745d9f6d3079da47b4cb9244477eb6088c95c36
Vraj Patel <39704588+ChocoMufn@users.noreply.github.com>
2021-10-13 21:59:54 -0400

    Trying to Call API Milestone

--- my-app/src/pages/Dashboard.js
+++ my-app/src/pages/Dashboard.js
@@ -3,8 +3,9 @@ import React, { Component } from "react";
 import { Table, Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 const PrintingCards = (notifications) => {
-    
+
     const makeCard = (card) => {
         return (
             <Card style={{ width: '18rem' }}>
@@ -13,16 +14,33 @@ const PrintingCards = (notifications) => {
                     {/* <Card.Title>{card.notification_type}</Card.Title>
                     <Card.Title>{card.notification_id}</Card.Title> */}
                     <Card.Text>{card.map()}</Card.Text>
-                    
+
                 </Card.Body>
             </Card>
         );
     };
 
-    return(
+    return (
         <div>{notifications.map(makeCard)}</div>
     );
 };
+const Test = (bearerToken) => {
+    const token = bearerToken;
+    const [result, setResult] = React.useState();
+    React.useEffect(() => {
+        fetch('https://7ktgzkl5ce.execute-api.us-east-1.amazonaws.com/prod/notifications', {
+            method: "GET",
+            headers: { "Authorization": `Bearer ${token}` }
+        }).then(res => res.json()).then(json => setResult(json));
+    }, []);
+
+    return (
+        <>
+            {(result)}
+        </>
+    );
+};
+
 export class Dashboard extends Component {
     constructor(props) {
         super(props);
@@ -34,10 +52,11 @@ export class Dashboard extends Component {
     render() {
         return (
             <div className="Dashboard">
-
+                {PrintingCards(Test(this.state.key))}
             </div>
         );
     }
+
 }
 
 export default Dashboard;
