ef545e3cd4cdf114dfc9f997ddbebdbf25dfddc7
Vraj Patel <39704588+ChocoMufn@users.noreply.github.com>
2021-10-13 20:35:42 -0400

    Milestone

--- my-app/src/pages/Dashboard.js
+++ my-app/src/pages/Dashboard.js
@@ -1,39 +1,45 @@
 import '../App.css';
 import React, { Component } from "react";
-import { Card, Button } from 'react-bootstrap';
+import { Table, Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const PrintingCards = () => {
+    
+    const makeCard = (card) => {
+        return (
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>{card.notification_type}</Card.Title>
+                    <Card.Title>{card.notification_id}</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    };
+
+    return(
+        <div></div>
+    );
+};
 export class Dashboard extends Component {
-	constructor(props) {
+    constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             user: this.props.user
         };
     }
-  render() {
-    return(
-        <div className="Dashboard">
-            <Card>
-                <Card.Title>Product</Card.Title>
-                <Table>
 
-                </Table>
-            </Card>
-            <Card>
-                <Card.Title>Contract</Card.Title>
-                <Table>
+    render() {
+        return (
+            <div className="Dashboard">
 
-                </Table>
-            </Card>
-            <Card>
-                <Card.Title>Message</Card.Title>
-                <Table>
-                    
-                </Table>
-            </Card>
-        </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default Dashboard;
