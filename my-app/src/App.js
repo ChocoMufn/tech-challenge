263ec628b7112fe2d3510d2586bc90c97bdd61b6
Vraj Patel <39704588+ChocoMufn@users.noreply.github.com>
2021-10-13 23:57:43 -0400

    API is working!!!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,7 +14,8 @@ export default class App extends Component {
       "FOR_CUSTOMER": {name: "Foreign Customer", key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVHlwZSI6IkZPUkVJR04ifQ.iTlQm0tUOJ4Boz72mOtsu3MXOgu7BOtR2dOG6xGSy00' },
     }
     this.state={
-      currentLogin: null
+      currentLogin: null,
+      users: []
     }
     this.updateLoginProfile = this.updateLoginProfile.bind(this)
   }
@@ -25,16 +26,43 @@ export default class App extends Component {
     console.log(this.profiles[profile])
   }
 
+  componentDidMount(){
+    var myHeaders = new Headers();
+    myHeaders.append("x-api-key", "AJalp94dfG14ju70pLJhzD0Hzpp7oOZ5egTmTEC1");
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "key": "val"
+    });
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("https://7ktgzkl5ce.execute-api.us-east-1.amazonaws.com/prod/users", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        this.setState({users: result});
+        console.log(result);
+      })
+      .catch(error => console.log('error', error));
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Login</h2>
-          <Button onClick={()=>this.updateLoginProfile("ADMIN")} variant="warning" className= "mb-3">Admin</Button>
-          <Button onClick={()=>this.updateLoginProfile("DOM_CUSTOMER")} variant="info" className= "mb-3">Domestic Customer</Button>
-          <Button onClick={()=>this.updateLoginProfile("FOR_CUSTOMER")} variant="info" className= "mb-3">Foreign Customer</Button>
-          {this.state.currentLogin ? <Dashboard user={this.state.currentLogin}/> : <></>}
+        
+          {this.state.currentLogin ? <Dashboard user={this.state.currentLogin}/> : 
+          <>
+            <h2>Login</h2>
+            <Button onClick={()=>this.updateLoginProfile("ADMIN")} variant="warning" className= "mb-3">Admin</Button>
+            <Button onClick={()=>this.updateLoginProfile("DOM_CUSTOMER")} variant="info" className= "mb-3">Domestic Customer</Button>
+            <Button onClick={()=>this.updateLoginProfile("FOR_CUSTOMER")} variant="info" className= "mb-3">Foreign Customer</Button>
+          </>}
         </header>
       </div>
     );
--- my-app/src/pages/Dashboard.js
+++ my-app/src/pages/Dashboard.js
@@ -24,35 +24,77 @@ const PrintingCards = (notifications) => {
         <div>{notifications.map(makeCard)}</div>
     );
 };
-const Test = (bearerToken) => {
-    const token = bearerToken;
-    const [result, setResult] = React.useState();
-    React.useEffect(() => {
-        fetch('https://7ktgzkl5ce.execute-api.us-east-1.amazonaws.com/prod/notifications', {
-            method: "GET",
-            headers: { "Authorization": `Bearer ${token}` }
-        }).then(res => res.json()).then(json => setResult(json));
-    }, []);
-
-    return (
-        <>
-            {(result)}
-        </>
-    );
-};
 
 export class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: this.props.user
+            user: this.props.user,
+            data: null,
+            newMessages: [],
+            newProduct: [],
+            newContract: [],
+            showMessages: true,
+            showContracts: true,
+            showProducts: true,
         };
+    }
+
+    componentDidMount(){
+            // GET request using fetch with set headers
+            var myHeaders = new Headers();
+            myHeaders.append("x-api-key", "AJalp94dfG14ju70pLJhzD0Hzpp7oOZ5egTmTEC1");
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("Access-Control-Request-Headers", "*");
+            myHeaders.append("Authorization", "Bearer " + this.state.user['key']);
+            
+
+            var requestOptions = {
+              method: 'GET',
+              headers: myHeaders,
+              mode: 'cors',
+            };
+            
+            fetch("https://7ktgzkl5ce.execute-api.us-east-1.amazonaws.com/prod/notifications", requestOptions)
+              .then(response =>{
+                  console.log(response)
+                  return response.json()})
+              .then(result => {
+                  result.forEach(data => {
+                      if(data["notification_type"] === "NEW_MESSAGE"){
+                        let newMessagesTemp = this.state.newMessages;
+                        newMessagesTemp.push(data);
+                        this.setState({newMessages: newMessagesTemp});
+                      }
+                      else if(data["notification_type"] === "NEW_PRODUCT"){
+                        let newProductTemp = this.state.newProduct;
+                        newProductTemp.push(data);
+                        this.setState({newProduct: newProductTemp});
+                      }
+                      else if(data["notification_type"] === "NEW_CONTRACT"){
+                        let newContractTemp = this.state.newContract;
+                        newContractTemp.push(data);
+                        this.setState({newContract: newContractTemp});
+                      }
+                  });
+                  this.setState({data: result});
+                  console.log(result)
+              })
+              .catch(error => console.log('error', error));
     }
 
     render() {
         return (
             <div className="Dashboard">
-                {PrintingCards(Test(this.state.key))}
+                {/* {PrintingCards(Test(this.state.key))} */}
+
+                {this.state.showContracts ? this.state.newContract.map(contract =>{
+                   return( 
+                   <Card>
+                        <p style={{color: "black"}}>{contract['notification_id']}</p>
+                    </Card>)
+                }) : <></>}
+
             </div>
         );
     }
